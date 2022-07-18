24be86fcde619fd4e4036c448feef7bb75706900
the me <alexanderzemans@gmail.com>
2022-07-18 08:20:18 -0700

    trolling

---
+++ ja.java
@@ -0,0 +1,8 @@
+public class CommandKit implements CommandExecutor {
+
+    // This method is called, when somebody uses our command
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        return false;
+    }
+}
